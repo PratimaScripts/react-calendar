5b6e8d81522eb90e2f16f1cd752ab9db7ad0f77c
Pratima Gautam <astute.yard@gmail.com>
2019-12-25 18:26:31 +0545

    Updatd display calendar modules

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {Component} from "react";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
 import "react-big-calendar/lib/css/react-big-calendar.css";
@@ -14,19 +14,46 @@ const App = () => (
         {
           title: "My event",
           allDay: false,
-          start: new Date(2018, 0, 1, 10, 0), // 10.00 AM
-          end: new Date(2018, 0, 1, 14, 0) // 2.00 PM
+          start: new Date(2019, 11, 1, 10, 0), // 10.00 AM
+          end: new Date(2019, 11, 2, 14, 0) // 2.00 PM
+        },
+        {
+          title: "Merry Christmas",
+          allDay: false,
+          start: new Date(2019, 11, 25, 10, 0), // 10.00 AM
+          end: new Date(2019, 11, 25, 14, 0) // 2.00 PM
         }
       ]}
       step={5}
       timeslots={3}
-      view="week"
-      views={["week"]}
-      min={new Date(2008, 0, 1, 8, 0)} // 8.00 AM
-      max={new Date(2008, 0, 1, 17, 0)} // Max will be 6.00 PM!
-      date={new Date(2018, 0, 1)}
+      view="month"
+      views={["month"]}
+      min={new Date(2019, 5, 1, 8, 0)} // 8.00 AM
+      max={new Date(2019, 11, 1, 17, 0)} // Max will be 6.00 PM!
+      // date={new Date(2019, 10, 1)}
     />
   </div>
 );
+
+// const myEventsList = {} //empty object for now
+// class App extends Component{
+//   constructor(props){
+//     super(props);
+//    //We will populate this function later
+//   }
+//   componentDidMount(){
+//    //We will populate this function later
+//   }
+//   render(){
+//     return(
+//       <Calendar
+//         localizer={localizer}
+//         events={[myEventsList]}
+//         startAccessor="start"
+//         endAccessor="end"
+//       />
+//     )
+//   }
+// }
 
 export default App;
