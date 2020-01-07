308268137bdad1b46d0e68b35d0a4f2a7a80bfc1
Pratima Gautam <astute.yard@gmail.com>
2020-01-07 10:57:04 +0545

    Added calendar_test.sql as a database backup with some revised changes

--- server/calendar_test.sql
+++ server/calendar_test.sql
@@ -1,3 +1,23 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 07, 2020 at 06:10 AM
+-- Server version: 10.4.10-MariaDB
+-- PHP Version: 7.3.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
 --
 -- Database: `calendar_test`
 --
@@ -20,4 +40,10 @@ CREATE TABLE `events` (
 --
 
 INSERT INTO `events` (`id`, `title`, `start`, `end`) VALUES
-(1, 'Post-Christmas Celebration', '2019-12-26 12:30:00', '2019-12-26 13:00:00');
+(1, 'Post-Christmas Celebration', '2019-12-26 06:45:00', '2019-12-26 07:15:00'),
+(2, 'Pre-New Year', '2019-12-28 11:41:00', '2019-12-29 12:41:00');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
